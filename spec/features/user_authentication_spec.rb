3de5f127521dfb90f2d344a72014a4f07872240c
KurtHsu <myohmy10420@gmail.com>
2018-05-12 02:00:46 +0800

    Add logout to root test

--- spec/features/user_authentication_spec.rb
+++ spec/features/user_authentication_spec.rb
@@ -15,4 +15,21 @@ RSpec.feature "User authentication" do
 
     expect(page).to have_text "user@example.com"
   end
+
+  scenario "user signs out" do
+    create(:user, email: "user@example.com", password: "password")
+
+    visit "/users/sign_in"
+
+    within(".new_user") do
+      fill_in "Email", with: "user@example.com"
+      fill_in "Password", with: "password"
+    end
+
+    click_button "Log in"
+
+    click_link "Logout"
+
+    expect(page).not_to have_text "user@example.com"
+  end
 end
