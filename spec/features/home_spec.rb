a8241d703a8b914043f458b9ed0216da6df058cb
KurtHsu <myohmy10420@gmail.com>
2018-05-12 01:54:59 +0800

    Add spec for root page has context 'Welcome'

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,6 +1,11 @@
 require "rails_helper"
 
 RSpec.describe "Home" do
+  scenario "welcomes user" do
+    visit root_url
+
+    expect(page).to have_text "Welcome"
+  end
   scenario "has navbar element" do
     visit root_url
 
