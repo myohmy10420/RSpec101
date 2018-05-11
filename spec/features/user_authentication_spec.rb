3452a7e08b1a075bbfda42c21d08c72493d57c34
KurtHsu <myohmy10420@gmail.com>
2018-05-12 01:57:51 +0800

    Add login to root test

---
+++ spec/features/user_authentication_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.feature "User authentication" do
+  scenario "existing user signs in" do
+    create(:user, email: "user@example.com", password: "password")
+
+    visit "/users/sign_in"
+
+    within(".new_user") do
+      fill_in "Email", with: "user@example.com"
+      fill_in "Password", with: "password"
+    end
+
+    click_button "Log in"
+
+    expect(page).to have_text "user@example.com"
+  end
+end
